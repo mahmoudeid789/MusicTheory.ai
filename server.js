2a2288145dc64d95d97295e360e48bbc1c86a2c8
Kadin Zhang <cykadin@gmail.com>
2019-02-14 06:08:01 -0500

    build for production

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-// var secure = require('express-force-https');
+var secure = require('express-force-https');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// app.use(secure);
+app.use(secure);
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, (req, res) => {
 	res.sendfile(__dirname + '/dist/index.html');
 });
 app.listen(port);
-
-// console.log('Server started...');
